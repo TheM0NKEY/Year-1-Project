0e79ce613af38dbdf1cc13e1cad7e533c5f1b1a6
The_M0nkey <jack.williams497@googlemail.com>
2013-11-05 13:49:10 +0000

    Buttons Added

--- Calculator Functions Model.py
+++ Calculator Functions Model.py
@@ -1,20 +1,2 @@
 # Calculator Functions model
 
-def decimal_cal():
-    """The function that handles the decimal part of the calculator"""
-
-    valA = int(input(""))
-    sign = input("")
-    valB = int(input(""))
-
-
-    if sign == "+":
-        result = valA + valB
-    elif sign == "-":
-        result = valA - valB
-    elif sign == "x":
-        result = valA * valB
-    elif sign == "/":
-        result == valA / valB
-
-    return result
--- Calculator.py
+++ Calculator.py
@@ -1,29 +1,55 @@
 from tkinter import *
 
+frame_index = 0
 
 class MainWindow(Frame):
 
-
-    def _init_(self, master):
+    def __init__(self, master):
         super(MainWindow, self).__init__(master)
         self.grid()
         self.create_widgets()
 
 
+
     def create_widgets(self):
-        self.decimalbutton = Button(self, text = "Decimal Calculator")
-        self.decimalbutton.grid()
-        self.binarybutton = Button(self, text = "Binary Calculator")
-        self.binarybutton.grid()
-        self.hexbutton = Button(self, text = "Hex Calculator")
-        self.hexbutton.grid()
+        global frame_index
+        self.decimalbutton = Button(self, text = "Decimal Calculator", command = self.dec_cal)
+        self.decimalbutton.grid(row = 0, column = 0)
+        self.binarybutton = Button(self, text = "Binary Calculator", command = self.bin_cal)
+        self.binarybutton.grid(row = 0, column = 2)
+        self.hexbutton = Button(self, text = "Hex Calculator", command = self.hex_cal)
+        self.hexbutton.grid(row = 0, column = 4)
+
+        if frame_index == 1:
+            self.inputbox = Entry(self)
+            self.inputbox.grid(row = 2, column = 2)
+        elif frame_index == 2:
+            self.inputbox = Entry(self)
+            self.inputbox.grid(row = 2, column = 2)
+        elif frame_index == 3:
+            self.inputbox = Entry(self)
+            self.inputbox.grid(row = 2, column = 2)
 
 
+    def dec_cal(self):
+        global frame_index
+        frame_index = 1
+        return frame_index
+
+    def bin_cal(self):
+        global frame_index
+        frame_index = 2
+        return frame_index
+
+    def hex_cal(self):
+        global frame_index
+        frame_index = 3
+        return frame_index
 
 
 
 root = Tk()
 root.title("Calculator")
-root.geometry("200x100")
+root.geometry("404x300")
 app = MainWindow(root)
 root.mainloop()
